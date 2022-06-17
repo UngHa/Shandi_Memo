e27cdffdcca8fb8d639fbe986c3f8b6b9f33fb26
UngHa <104884079+UngHa@users.noreply.github.com>
2022-06-18 07:00:39 +0900

    캐릭터목록 보이겧ㅁㄴ이ㅓㅎ

--- app/src/main/java/com/example/shandi_memo/CharacterListAdapter.java
+++ app/src/main/java/com/example/shandi_memo/CharacterListAdapter.java
@@ -1,5 +1,6 @@
 package com.example.shandi_memo;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +14,13 @@ import java.util.ArrayList;
 
 public class CharacterListAdapter extends RecyclerView.Adapter<CharacterListAdapter.ViewHolder>  {
     ArrayList<CharacterItem> items = new ArrayList<CharacterItem>();
-
+    Context context;
     OnTapItemSelectedListener listener;
 
-    int layoutType = 0;
-
-
+    public CharacterListAdapter(ArrayList<CharacterItem> items, Context context) {
+        this.items = items;
+        this.context = context;
+    }
 
     @NonNull
     @Override
@@ -31,15 +33,66 @@ public class CharacterListAdapter extends RecyclerView.Adapter<CharacterListAdap
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        CharacterItem item = items.get(position);
-        holder.setItem(item);
+        if (holder.className.getText() == "디스트로이어") {
+            holder.profileIcon.setImageResource(R.drawable.destroyer);
+        } else if (holder.className.getText() == "워로드") {
+            holder.profileIcon.setImageResource(R.drawable.warlord);
+        } else if (holder.className.getText() == "버서커") {
+            holder.profileIcon.setImageResource(R.drawable.berserker);
+        } else if (holder.className.getText() == "홀리나이트") {
+            holder.profileIcon.setImageResource(R.drawable.holyknight);
+        } else if (holder.className.getText() == "배틀마스터") {
+            holder.profileIcon.setImageResource(R.drawable.battlemaster);
+        } else if (holder.className.getText() == "인파이터") {
+            holder.profileIcon.setImageResource(R.drawable.infighter);
+        } else if (holder.className.getText() == "기공사") {
+            holder.profileIcon.setImageResource(R.drawable.soulmaster);
+        } else if (holder.className.getText() == "창술사") {
+            holder.profileIcon.setImageResource(R.drawable.lancemaster);
+        } else if (holder.className.getText() == "스트라이커") {
+            holder.profileIcon.setImageResource(R.drawable.striker);
+        } else if (holder.className.getText() == "데빌헌터") {
+            holder.profileIcon.setImageResource(R.drawable.devilhunter);
+        } else if (holder.className.getText() == "블래스터") {
+            holder.profileIcon.setImageResource(R.drawable.blaster);
+        } else if (holder.className.getText() == "호크아이") {
+            holder.profileIcon.setImageResource(R.drawable.hawkeye);
+        } else if (holder.className.getText() == "스카우터") {
+            holder.profileIcon.setImageResource(R.drawable.scouter);
+        } else if (holder.className.getText() == "건슬링어") {
+            holder.profileIcon.setImageResource(R.drawable.gunslinger);
+        } else if (holder.className.getText() == "바드") {
+            holder.profileIcon.setImageResource(R.drawable.bard);
+        } else if (holder.className.getText() == "서머너") {
+            holder.profileIcon.setImageResource(R.drawable.summoner);
+        } else if (holder.className.getText() == "아르카나") {
+            holder.profileIcon.setImageResource(R.drawable.arcanist);
+        } else if (holder.className.getText() == "소서리스") {
+            holder.profileIcon.setImageResource(R.drawable.sorceress);
+        } else if (holder.className.getText() == "데모닉") {
+            holder.profileIcon.setImageResource(R.drawable.demonic);
+        } else if (holder.className.getText() == "블레이드") {
+            holder.profileIcon.setImageResource(R.drawable.blade);
+        } else if (holder.className.getText() == "리퍼") {
+            holder.profileIcon.setImageResource(R.drawable.reaper);
+        } else if (holder.className.getText() == "도화가") {
+            holder.profileIcon.setImageResource(R.drawable.artist);
+        }
+        holder.charName.setText(items.get(position).getName());
+        holder.className.setText(items.get(position).getClassName());
+        holder.ItemLv.setText(items.get(position).getLevel());
+        context = holder.itemView.getContext();
+
+        //CharacterItem item = items.get(position);
+       // holder.setItem(item);
     }
 
     @Override
     public int getItemCount() {
         return items.size();
     }
-    public void addItem(CharacterItem item){
+
+    /*public void addItem(CharacterItem item){
         items.add(item);
     }
 
@@ -59,7 +112,7 @@ public class CharacterListAdapter extends RecyclerView.Adapter<CharacterListAdap
         if(listener != null){
             listener.onItemClick(holder, view, position);
         }
-    }
+    }*/
 
     static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView profileIcon;
@@ -90,9 +143,9 @@ public class CharacterListAdapter extends RecyclerView.Adapter<CharacterListAdap
 
         public void setItem(CharacterItem item){
             profileIcon.setImageResource(R.drawable.ic_launcher_foreground);
-            charName.setText(item.getCharName());
+            charName.setText(item.getName());
             className.setText(item.getClassName());
-            ItemLv.setText(item.getItemLevel());
+            ItemLv.setText(item.getLevel());
         }
     }
 }
